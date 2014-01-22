cbdcb7d3f0a25b7df635a40048a653728b90d74b
Florian Giroud <fgiroud@onisep.fr>
2014-01-22 11:58:29 +0100

    sql

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,8 @@
 # DOUBLONS
-
 SELECT *, COUNT(*) AS count  
 FROM table
 GROUP BY field_with_duplicate
 HAVING COUNT(*) > 1
+
+# RESET TABLE
+UPDATE table_name t SET t.email=CONCAT('test_', t.id, 'custom_value');
