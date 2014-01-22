b76f42147abaa1c2c9a8c3775eeaa55ef0847264
Florian Giroud <fgiroud@onisep.fr>
2014-01-22 16:36:41 +0100

    sql

--- SQL.sql
+++ SQL.sql
@@ -6,3 +6,26 @@ HAVING COUNT(*) > 1
 
 # RESET TABLE
 UPDATE table_name t SET t.email=CONCAT('test_', t.id, 'custom_value');
+
+
+# RECORD WITHOUT JOIN
+SELECT d.*, COUNT(*) FROM tablename d 
+WHERE d.id NOT IN (SELECT join_column FROM join_table) 
+GROUP BY d.id
+
+
+# COUNT RECORD WITHOUT JOIN + GROUP BY
+SELECT t.nom, COUNT(*) FROM diplomes d 
+LEFT JOIN type_diplome t ON t.id=d.type_diplome_id
+WHERE d.id NOT IN (SELECT diplome_id FROM competences)
+AND t.is_active=1
+GROUP BY type_diplome_id
+
+
+# GROUP CONCAT
+GROUP_CONCAT( f1, f2 SEPARATOR "---" ) AS f;
+
+
+# FOREIGN KEYS CONSTRAINTS CHECK
+SET foreign_key_checks = 0;
+SET foreign_key_checks = 1;
