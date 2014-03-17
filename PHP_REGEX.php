c33d37273aa2686ab5a9d4f261ca7609077373d5
Florian Giroud <yomotchide@gmail.com>
2014-03-17 12:35:10 +0700

    Update PHP_REGEX.php

--- PHP_REGEX.php
+++ PHP_REGEX.php
@@ -1,3 +1,11 @@
 <?php
 // tous les chaines qui commencent par {% et finissent par %}
 $regex = "#{%(.*?)%}#";
+
+
+// extraction de liens avec une classe précise
+preg_match_all('#<a class="css-class" href="(.*)">(.*)</a>#', $source, $matches);
+
+
+// extraction d'url
+preg_match('#href="([^\s"]+)#', $link, $matches);
