181475c5e4c69c4f32f832f43d0ed49409bb0dc5
Florian Giroud <yomotchide@gmail.com>
2014-04-03 19:46:01 +0700

    Update PHP.php

--- PHP.php
+++ PHP.php
@@ -1,1 +1,17 @@
 <?php
+
+
+// Simple PDo connection
+
+$dns = 'mysql:host=localhost;dbname=logiciel';
+$user = 'root';
+$pwd = '123';
+$connection = new PDO( $dns, $user, $pwd );
+
+$query = 'SELECT * FROM clone_toocharger_categories';
+$select = $connection->query($query);
+$records=$select->fetchAll(PDO::FETCH_OBJ);
+
+foreach($records as $record){
+    var_dump($record);
+}
