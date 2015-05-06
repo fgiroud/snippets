5d46bf70879e5e2141879ff51c6c80631887adb1
Florian Giroud <florian.giroud@gmail.com>
2015-05-06 09:45:33 +0200

    Update GIT.bash

--- GIT.bash
+++ GIT.bash
@@ -4,3 +4,8 @@ git log preprod ^master --no-merges
 
 #annuler le dernier commit non propage
 git reset HEAD^
+
+
+
+#purge sal dump from particular table inserts
+sed '/INSERT INTO `TABLE_TO_SKIP`/d' DBdump.sql > reduced.sql
