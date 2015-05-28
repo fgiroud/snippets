c80f2904285eaddd4d97e1a5e11fcf594605dddb
Florian Giroud <florian.giroud@gmail.com>
2015-05-28 15:03:34 +0700

    Update UNIX.sh

--- UNIX.sh
+++ UNIX.sh
@@ -3,3 +3,6 @@ source ~/.bash_profile
 
 // search all files containing "seach"
 grep -lR "seach" ./
+
+// exclude table from mysqldump
+sed '/INSERT INTO `TABLE_TO_SKIP`/d' DBdump.sql > reduced.sql
