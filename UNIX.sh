2d8b53754bb4f32a963e71be9db12a0f46a07a42
Florian Giroud <6267288+fgiroud@users.noreply.github.com>
2017-11-16 13:21:24 +0700

    Update UNIX.sh

--- UNIX.sh
+++ UNIX.sh
@@ -6,3 +6,6 @@ grep -lR "seach" ./
 
 // exclude table from mysqldump
 sed '/INSERT INTO `TABLE_TO_SKIP`/d' DBdump.sql > reduced.sql
+
+// FInd multiple files, in multiple folders, and count lines
+find -E ./app ./src ./doc -regex '.*\.(yml|php|feature|twig|md)' -exec cat {} \;| wc -l
